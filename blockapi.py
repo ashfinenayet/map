00c488d1f650da069565765b73017735205071a9
ashfinenayet <ashfin.enayet@gmail.com>
2022-01-13 13:24:19 -0500

    messing around

--- blockapi.py
+++ blockapi.py
@@ -1,8 +1,12 @@
-import urllib, json, requests
+import urllib
+import json
+import requests
 import urllib.request
+import pandas as pd
 
-
-
+df = pd.read_csv('urbanization-census-tract.csv')
+latitude = df['lat_tract']
+longitude = df['long_tract']
 
 with urllib.request.urlopen("https://geo.fcc.gov/api/census/block/find?latitude=32.47718&longitude=-86.49007&format=json") as url:
     data = json.loads(url.read().decode())
