c38af8f956e1b02148559225f3d44407ceff721f
ashfinenayet <ashfin.enayet@gmail.com>
2022-02-19 13:49:34 -0500

    added new files changed name etc

--- blockapi.py
+++ blockapi.py
@@ -1,34 +1,54 @@
-import numpy as np
-import urllib
-import json
 import requests
-import urllib.request
 import pandas as pd
-from rich.progress import track 
-results = []
-df = pd.read_csv('urbanization-census-tract.csv')
-lat = df['lat_tract'].astype(float)
-lon = df['long_tract'].astype(float)
-for i in track(range(0, len(df))): #iterate each row for API
-    
-    row = df.iloc[i]
-# Encode parameters
-    params = urllib.parse.urlencode(
-    {'latitude': row['lat_tract'], 'longitude': row['long_tract'], 'format': 'json'})
-    # Contruct request URL
-    url = 'https://geo.fcc.gov/api/census/block/find?' + params
+from typing import cast
 
-# Get response from API
-    response = requests.get(url)
+results = []
+
+READABLE_FILE_PARAMS = {
+    "file_okay": True,
+    "dir_okay": False,
+    "path_type": "Path",
+}
+
+
+def get_fips_data(row) -> str:
+    """Get FIPS data for a row in the dataframe column.
+    We constrain this operation to a method so we can map over each row in the
+    DataFrame.
+    Args:
+        row: The row to retrieve FIPS information for.
+    Returns:
+       The FIPS string for the given row.
+    """
+    url = "https://geo.fcc.gov/api/census/block/find"
+
+    # Get response from API
+    payload = {
+        "latitude": row["lat_tract"],
+        "longitude": row["long_tract"],
+        "format": "json",
+    }
+    response = requests.get(url, params=payload)
 
     # Parse json in response
     data = response.json()
-    results.append(data['County']['FIPS'])
-    breakpoint()
-
-# Print FIPS code
+    return data["County"]["FIPS"]
 
 
-df['FIPS'] = np.array(data)
+def augment_fips() -> None:
+    """Augment a CSV with FIPS information.
+    This script will read from INPUT_FNAME and write the augmented CSV to OUTPUT_FNAME.
+    """
+    input_fname = "urbanization-census-tract.csv"
+    output_fname = "urbanization-census-tract-updated.csv"
+    df = cast(pd.DataFrame, pd.read_csv(input_fname))
+    df["lat_tract"] = df["lat_tract"].astype(float)
+    df["long_tract"] = df["long_tract"].astype(float)
 
-df.to_csv('urbanization-census-tract-updated', sep='\t')
+    # Map over each row to get FIPS data.
+    df["FIPS"] = df.apply(get_fips_data, axis=1)
+    df.to_csv(output_fname)
+
+
+if __name__ == "__main__":
+    augment_fips()
