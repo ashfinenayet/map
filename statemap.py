49d7b22e3668aa9dfc5e9eea4886769cf139dd91
ashfinenayet <ashfin.enayet@gmail.com>
2022-02-19 17:29:03 -0500

    added title

--- statemap.py
+++ statemap.py
@@ -4,8 +4,7 @@ df = pd.read_csv('urbanization-state.csv')
 df.columns = map(str.strip, df.columns)
 df['ab'] = df['ab'].apply(str.strip)
 df['urbanindex'] = df['urbanindex'].astype(float)
-print(df)
-data_top = df.head() 
+
 fig = go.Figure(data=go.Choropleth(
     locations=df['ab'], # Spatial coordinates
     z = df['urbanindex'].astype(float), # Data to be color-coded
@@ -17,12 +16,14 @@ fig = go.Figure(data=go.Choropleth(
 ))
 
 fig.update_layout(
+    title_text = 'Which States are the Most Urban?',
    geo = dict(
         scope='usa',
         projection=go.layout.geo.Projection(type = 'albers usa'),
         showlakes=True, # lakes
         lakecolor='rgb(255, 255, 255)'),
-)
+),
+
 
 
 fig.show()
