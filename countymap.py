97a404dbc1a2e19d19d3f252e53bab64e546f5d8
ashfinenayet <ashfin.enayet@gmail.com>
2022-02-19 17:28:49 -0500

    fixed map added population on hover made it mapbox format

--- countymap.py
+++ countymap.py
@@ -1,14 +1,22 @@
 
 import plotly.express as px
-
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
 import pandas as pd
-df = pd.read_csv('urbanization-census-tract.csv')
+df = pd.read_csv('urbanization-census-tract-updated.csv',  dtype={"FIPS": str}) #Make FIPS string so it can be read properly and avoid holes 
 
-fig = px.scatter_geo(df,  lon='long_tract', lat='lat_tract', color='urbanindex',
-                     hover_name='state',
-                     hover_data=['population'],
-                     scope="usa",
-                           labels={'Urban': 'urban index'}
-                     )
-fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+import plotly.express as px
+
+fig = px.choropleth_mapbox(df, title = 'most urban counties',  template='plotly_dark', geojson=counties, locations='FIPS', color='urbanindex',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12.5),
+                           mapbox_style="carto-darkmatter",
+                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.5,
+                           hover_data=  ['population'],
+                           labels={'urbanindex':'urban index'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
